537f6be4d40d2a1e6c06650180e773a13cd27298
PixelBedrock <113065795+PixelBedrock@users.noreply.github.com>
2025-03-26 13:43:09 +0000

    projec native

---
+++ src/components/Select.tsx
@@ -0,0 +1,27 @@
+"use dom";
+
+type IOption = {
+  default?: boolean;
+  id: string;
+  text: string;
+};
+
+type ISelect = {
+  options: IOption[];
+  onChange: React.ChangeEventHandler<HTMLSelectElement>;
+};
+
+export default function Select({ options, onChange }: ISelect) {
+  return (
+    <select
+      defaultValue={options.filter((opt) => opt.default)[0].id}
+      onChange={onChange}
+    >
+      {options.map((value, index) => (
+        <option key={index} value={value.id}>
+          {value.text}
+        </option>
+      ))}
+    </select>
+  );
+}
